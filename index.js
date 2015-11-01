b2c793bc560a43f2b41fc5f091223d0a587f4309
daniel-llach <daniel.llach@gmail.com>
2015-11-01 03:58:09 -0300

    listen new tags file and rename version on package.json

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 var fs = require("fs-extra");
+// var path = require("path");
 
-// Asynchronous read
-fs.readJson('./package.json', function(err, packageObj) {
-  if(err) {
-    console.log("Error: ", "Please check read file permissions. Try: sudo chmod 777 package.json");
-  }else{
-    // get version
-    var version = '0.0.2';
-    // set version
-    packageObj.version = version;
-    // write new package object
-    fs.writeJson('./package.json', packageObj, function(err){
-      if(err) {
-        console.log("Error: ", "Please check write file permissions. Try: sudo chmod 777 package.json");
-      }else{
-        console.log(packageObj.name + " update to version " + packageObj.version);
-      }
-    });
+var path = './.git/refs/tags';
+
+// watch tags directory for a new one
+fs.watch(path, getNewVersion);
+
+function getNewVersion(event,file){
+  console.log('event is: ' + event);
+  if (file) {
+    var newVersion = file.split("v");
+    readPackage(newVersion[1]);
+  } else {
+    console.log('filename not provided');
   }
-});
+}
+
+function readPackage(version){
+
+  fs.readJson('./package.json', function(err, packageObj) {
+    if(err) {
+      console.log("Error: ", "Please check read file permissions. Try: sudo chmod 777 package.json");
+    }else{
+      // set version
+      packageObj.version = version;
+      // write new package object
+      fs.writeJson('./package.json', packageObj, function(err){
+        if(err) {
+          console.log("Error: ", "Please check write file permissions. Try: sudo chmod 777 package.json");
+        }else{
+          console.log(packageObj.name + " update to version " + packageObj.version);
+        }
+      });
+    }
+  });
+
+
+}
